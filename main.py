55a2786c7961050075c76be370f2825508421025
HaltTheGrey <your-email@example.com>
2025-10-20 21:33:57 -0700

    this is my forth commit... I forgot to add this file to my last commit hehelol... this is what I actually wanted to add

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-import customtkinter
-import os
-import sys
+from typing import Tuple
+import customtkinter as ctk
+from datetime import datetime
+import threading
+import time
 
 """
 this is my sandbox for all of my thoughts, AI will help me sort out and teach me all the necessary
 tools I need to be a successful programmer. This will be my first official commit.
+
+first project I will make a personal task timer with notification
 """
+
+class PomdoroTimer(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        # Window makeup
+        
+        self.title("focus timer")
+        self.geometry("400x300")
+        
+        # Timer state
+        
+        self.time_left = 25 * 60
+        self.is_running = False
+        
+        # TODO: Create you UI elements here
+        
+    def Start_timer(self):
+        # TODO: Implement timer logic
+        pass
+    
+    def reset_timer(self):
+        # TODO: Reset to default time
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    app = PomdoroTimer()
+    app.mainloop() 
