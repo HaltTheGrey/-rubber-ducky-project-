ffffcda29c48949803bed3397d22cd8745dcc54e
HaltTheGrey <your-email@example.com>
2025-10-20 21:02:31 -0700

    this is my seccond commit so far: testing testing 123!

---
+++ main.py
@@ -0,0 +1,8 @@
+import customtkinter
+import os
+import sys
+
+"""
+this is my sandbox for all of my thoughts, AI will help me sort out and teach me all the necessary
+tools I need to be a successful programmer. This will be my first official commit.
+"""
